3fd031ff302e6980c023ad1d083071a0e78b69b1
Tyler Townsend <townsend.ts@outlook.com>
2023-04-15 15:55:16 -0700

    integrated stream chat

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use aita::ai::open_ai_gpt::{GptClient, ClientRequest, STOP_PHRASE};
+use aita::render::chat_mode::{self, run_chat_mode};
 use clap::{App, Arg, SubCommand};
 use std::env;
 use std::io::{self, Write};
@@ -37,41 +38,31 @@ async fn main() {
         .subcommand(SubCommand::with_name("chat").about("Enter chat mode with the GPT API"))
         .get_matches();
 
-    let mut gpt_client = GptClient::new(api_key);
+    let mut gpt_client = GptClient::new(api_key, 1000, 1, 0.8, String::from("gpt-3.5-turbo"), None);
 
     if let Some(search_matches) = matches.subcommand_matches("search") {
         if let Some(query) = search_matches.value_of("query") {
             let request = ClientRequest {
                 prompt: query.to_string(),
-                max_tokens: 100,
-                n: 1,
-                temperature: 0.8,
-                model: String::from("text-davinci-003"),
                 chat_log: None, // No chat log needed for search
-                stop: None, // No stop phrase needed for search
             };
     
-            match gpt_client.generate_response(request).await {
-                Ok(response) => println!("GPT response: {}", response),
-                Err(e) => eprintln!("Error: {}", e),
-            }
+            // match gpt_client.generate_response(request).await {
+            //     Ok(response) => println!("GPT response: {}", response),
+            //     Err(e) => eprintln!("Error: {}", e),
+            // }
         }
     } else if let Some(debug_matches) = matches.subcommand_matches("debug") {
         if let Some(error_message) = debug_matches.value_of("error") {
             let request = ClientRequest {
                 prompt: format!("How to fix the following error in code: {}", error_message),
-                max_tokens: 100,
-                n: 1,
-                temperature: 0.8,
-                model: String::from("text-davinci-003"),
                 chat_log: None, // No chat log needed for debug
-                stop: Some(String::from("import")), // Stop on "import" keyword
             };
     
-            match gpt_client.generate_response(request).await {
-                Ok(response) => println!("GPT response: {}", response),
-                Err(e) => eprintln!("Error: {}", e),
-            }
+            // match gpt_client.generate_response(request).await {
+            //     Ok(response) => println!("GPT response: {}", response),
+            //     Err(e) => eprintln!("Error: {}", e),
+            // }
         } else {
             // Terminal capture code here
             println!("Terminal capture not implemented yet");
@@ -86,39 +77,13 @@ async fn main() {
             Vec::new()
         };
 
-        loop {
-            print!("You: ");
-            io::stdout().flush().unwrap();
-            let mut input = String::new();
-            io::stdin().read_line(&mut input).unwrap();
-            input = input.trim().to_string();
-
-            if input.to_lowercase() == "chat::quit" {
-                break;
-            }
-
-            let request = ClientRequest {
-                prompt: input.clone(),
-                max_tokens: 1000,
-                n: 1,
-                temperature: 0.8,
-                model: String::from("text-davinci-003"),
-                chat_log: Some(chat_history.clone()), // Update this line
-                stop: Some(String::from(STOP_PHRASE)),
-            };
-            match gpt_client.generate_response(request).await {
-                Ok(response) => {
-                    let output = format!("GPT: {}", response);
-                    println!("{}", output);
-                    chat_history.push(format!("You: {}", input));
-                    chat_history.push(output);
-                }
-                Err(e) => eprintln!("Error: {}", e),
-            }
+        match run_chat_mode(gpt_client).await {
+            Ok(_) => println!("Chat ended."),
+            Err(e) => eprintln!("Error: {}", e),
         }
 
-        File::create(chat_history_path)
-            .and_then(|mut file| file.write_all(chat_history.join(STOP_PHRASE).as_bytes()))
-            .expect("Failed to save chat history");
+        // File::create(chat_history_path)
+        //     .and_then(|mut file| file.write_all(chat_history.join(STOP_PHRASE).as_bytes()))
+        //     .expect("Failed to save chat history");
     }
 }
