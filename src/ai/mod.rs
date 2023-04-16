f962b7cc97cc58ff0a3ae792adb4257cda6ad8f4
Tyler Townsend <townsend.ts@outlook.com>
2023-04-15 19:50:13 -0700

    add different rendering

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -2,9 +2,15 @@ pub mod open_ai_gpt;
 
 use std::error::Error;
 use std::io::{stdout, Write};
-use async_openai::types::{ChatCompletionRequestMessage, Role, ChatCompletionRequestMessageArgs};
+use async_openai::types::ChatCompletionRequestMessage;
 use futures::stream::StreamExt;
-use hyper::http::response;
+use termimad::{MadSkin};
+// use ansi_term::{Style, Color};
+use crossterm::{
+    execute,
+    style::{Color, Print, ResetColor, SetForegroundColor, SetBackgroundColor, SetAttribute, Attribute},
+};
+
 
 use self::open_ai_gpt::{GptClientRequest, GptClient};
 
@@ -13,15 +19,34 @@ pub struct ChatClient {
     chat_log: Vec<ChatCompletionRequestMessage>
 }
 
+
+struct TextState {
+    in_code_block: bool,
+    buffer: String,
+    backticks_count: i32
+}
+
+impl TextState {
+    pub fn new() -> Self {
+        Self { in_code_block: false, buffer: String::new(), backticks_count: 0 }
+    }
+}
+
 impl ChatClient {
     pub fn new(gpt_client: GptClient) -> Self {
         Self { gpt_client, chat_log: Vec::new() }
     }
 
-    pub async fn render_response(
-        &mut self,
-        user_input: String,
-    ) -> Result<String, Box<dyn Error>> {
+    pub async fn render_input(&mut self) -> Result<String, Box<dyn Error>> {
+        // let mut lock = stdout().lock();
+        execute!(std::io::stdout(), SetForegroundColor(Color::Cyan), SetAttribute(Attribute::Bold), Print("You: "), ResetColor).unwrap();
+        std::io::stdout().flush().unwrap();
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        Ok(input.trim().to_string())
+    }
+
+    pub async fn render_response(&mut self, user_input: String) -> Result<String, Box<dyn Error>> {
         let mut response_string = String::new();
         let mut lock = stdout().lock();
 
@@ -31,19 +56,26 @@ impl ChatClient {
 
         let mut stream = self.gpt_client.generate_response(&client_request).await?;
 
-        print!("\nAI: ");
+        let mut state = TextState::new();
+        execute!(lock, SetForegroundColor(Color::Green), SetAttribute(Attribute::Bold), Print("AI : "), ResetColor).unwrap();
         while let Some(result) = stream.next().await {
             match result {
                 Ok(response) => {
                     response.choices.iter().for_each(|chat_choice| {
                         if let Some(ref content) = chat_choice.delta.content {
-                            write!(lock, "{}", content).unwrap();
+                            self.print_content(& mut lock, content, & mut state).unwrap();
                             response_string.push_str(content);
                         }
                     });
                 }
                 Err(err) => {
-                    writeln!(lock, "error: {}", err).unwrap();
+                    execute!(
+                        lock,
+                        SetForegroundColor(Color::Red),
+                        Print(format!("error: {}", err)),
+                        ResetColor
+                    )
+                    .unwrap();
                 }
             }
             stdout().flush()?;
@@ -54,19 +86,31 @@ impl ChatClient {
             .create_assistant_message(response_string.clone())?;
         self.chat_log.push(response_message);
         Ok(response_string)
-        // match result {
+    }
 
-        //     Ok(assistant_prompt) => {
-        //         let response_message = self.gpt_client
-        //             .clone()
-        //             .create_assistant_message(assistant_prompt)?;
-        //         self.chat_log.push(response_message);
-        //         result
-        //     }
-        //     Err(err) => {
-        //         writeln!(lock, "error: {}", err).unwrap();
-        //         Err(err)
-        //     }
-        // }
+    fn print_content(&mut self, lock: &mut std::io::StdoutLock, content: &str, state: &mut TextState) -> Result<(), Box<dyn Error>> {
+        for c in content.chars() {
+            if c == '`' {
+                state.backticks_count += 1;
+            } else {
+                state.backticks_count = 0;
+            }
+
+            if state.backticks_count == 3 {
+                state.backticks_count = 0;
+                state.in_code_block = !state.in_code_block;
+                if !state.in_code_block {
+                    execute!(lock, ResetColor).unwrap();
+                }
+                continue;
+            }
+
+            if !state.in_code_block || state.backticks_count > 0 {
+                write!(lock, "{}", c).unwrap();
+            } else {
+                execute!(lock, SetForegroundColor(Color::DarkGreen), SetBackgroundColor(Color::Black), Print(c), ResetColor).unwrap();
+            }
+        }
+        Ok(())
     }
 }
--- src/render/chat_mode/mod.rs
+++ src/render/chat_mode/mod.rs
@@ -5,35 +5,15 @@ use crate::ai::{open_ai_gpt::{GptClient, GptClientRequest}, ChatClient};
 
 pub async fn run_chat_mode(gpt_client: GptClient) -> Result<(), Box<dyn Error>> {
     let mut chat_client = ChatClient::new(gpt_client);
-    let mut chat_log: Vec<ChatCompletionRequestMessage> = Vec::new();
-
-    // let mut prompt = String::new();
-    // let stdin = io::stdin();
-    // let mut stdout = io::stdout();
 
     loop {
-        print!("You: ");
-        io::stdout().flush().unwrap();
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-        input = input.trim().to_string();
+        let input = chat_client.render_input().await?;
 
         if input.trim() == "stop" {
             break;
         }
 
-
         let response = chat_client.render_response(input).await?;
-
-
-        // User should be added via render_response
-        // let user_message = ChatCompletionRequestMessageArgs::default()
-        //     .content(input.to_string())
-        //     .role(Role::User)
-        //     .build()?;
-        // chat_log.push(user_message);
-
-        // prompt.clear();
     }
     Ok(())
 }
