19092d03ae11e23836489bb65e3ade7b607617a7
Tyler Townsend <townsend.ts@outlook.com>
2023-06-07 07:28:53 -0700

    restyle the config menu for a model

--- src/execution/config_menu/view_model.rs
+++ src/execution/config_menu/view_model.rs
@@ -2,10 +2,10 @@ use crossterm::event::{self, Event, KeyCode, KeyEventKind};
 use std::{io, error::Error};
 use tui::{
     backend::CrosstermBackend,
-    layout::{Constraint, Direction, Layout, Alignment},
+    layout::{Constraint, Direction, Layout, Alignment, Corner},
     style::{Color, Modifier, Style},
     text::{Span, Spans},
-    widgets::{Block, Borders, Paragraph},
+    widgets::{Block, Borders, Paragraph, List, ListItem},
     Terminal,
 };
 
@@ -53,191 +53,92 @@ fn view_config(
         .keys()
         .map(|key| key.to_string())
         .collect();
-    let mut config_widget: Vec<String> = config_fields
+    let config_widgets: Vec<String> = config_fields
         .iter()
         .map(|field| {
             get_config_value_as_string(&config[field])
         })
         .collect();
 
-    let mut selected_field = 0;
-    let mut editing_field = false;
-    let mut unsaved_changes = false;
-    let scroll_offset = 0;
+    let mut state = MenuState {
+        config_fields,
+        config_widgets,
+        required_config,
+        editing_field: false,
+        scroll_offset: 0,
+        selected_field: 0,
+        unsaved_changes: false
+    };
 
     loop {
-        terminal.draw(|frame| {
-            let size = frame.size();
-            let block = Block::default()
-                .borders(Borders::ALL)
-                .title("View Model")
-                .style(Style::default()
-                    .fg(Color::White)
-                    .add_modifier(Modifier::BOLD));
-            frame.render_widget(block, size);
-    
-
-            // -----------------------------
-            // Content on top of title block
-            // -----------------------------
-            let chunks = Layout::default()
-                .direction(Direction::Vertical)
-                .margin(1)
-                .constraints([
-                    Constraint::Length(5),
-                    Constraint::Length(15),
-                    Constraint::Length(3),
-                ])
-                .split(size);
-
-            // static fields
-            let formatted_string = format!("{} ({})", required_config.name, required_config.id);
-            let question =Paragraph::new(Spans::from(vec![Span::raw(formatted_string)]))
-                .style(Style::default().fg(Color::White))
-                .alignment(Alignment::Left)
-                .block(Block::default()
-                    .border_style(Style::default().fg(Color::White))
-                    .borders(Borders::ALL)
-                    .title(Span::styled("\nModel", Style::default()
-                        .fg(Color::Cyan)
-                        .add_modifier(Modifier::BOLD))
-                ));
-            frame.render_widget(question, chunks[0]);
-
-
-            // dynamic fields
-            let mut field_list = Vec::new();
-            let num_displayed_fields = std::cmp::min(5, config_fields.len() - scroll_offset);
-            for (i, (label, input_widget)) in config_fields.iter()
-                                                                            .skip(scroll_offset)
-                                                                            .take(num_displayed_fields)
-                                                                            .zip(config_widget
-                                                                                .iter_mut()
-                                                                                .skip(scroll_offset)
-                                                                                .take(num_displayed_fields))
-                                                                            .enumerate() {
-                let input_block = Block::default()
-                    .title(Span::styled(label, Style::default().fg(Color::White)))
-                    .borders(Borders::ALL);
-                let mut input_style = Style::default().fg(Color::White);
-                if editing_field && i + scroll_offset == selected_field {
-                    input_style = input_style.bg(Color::DarkGray);
-                }
-                let input_text = if editing_field && i + scroll_offset == selected_field {
-                    format!("{}|", input_widget.as_str())
-                } else {
-                    input_widget.to_string()
-                };
-                let input = Paragraph::new(input_text)
-                    .style(input_style)
-                    .block(input_block);
-                field_list.push(input)
-            }
-            let field_chunks = Layout::default()
-                .direction(Direction::Vertical)
-                .constraints(
-                    std::iter::repeat(Constraint::Length(3))
-                        .take(num_displayed_fields) // Limit to 5 displayed fields
-                        .collect::<Vec<_>>()
-                        .as_ref(),
-                )
-                .split(chunks[1]);
-
-            for (i, field) in field_list.into_iter().enumerate() {
-                frame.render_widget(field, field_chunks[i]);
-            }
-
-            // actions
-            let action_text = if editing_field {
-                "[S] Save [ESC] Stop Editing [B] Back"
-            } else {
-                "[E] Edit [B] Back"
-            };
-            let action_span = Span::styled(action_text, Style::default().fg(Color::White));
-            let action_block = Block::default().title(action_span).borders(Borders::ALL);
-            let action_paragraph_text = if editing_field {
-                "Editing"
-            } else {
-                ""
-            };
-            let action_paragraph_text_color = if editing_field {
-                Color::Yellow
-            } else {
-                Color::White
-            };
-            let action_paragraph = Paragraph::new(action_paragraph_text)
-                .style(Style::default().fg(action_paragraph_text_color))
-                .block(action_block);
-            frame.render_widget(action_paragraph, chunks[2]);
-        })?;
-
+        present_menu(terminal, &mut state)?;
         match event::read()? {
             Event::Key(event) => match event.kind {
                 KeyEventKind::Press => match event.code {
                     KeyCode::Char('s') | KeyCode::Char('S') => {
-                        if editing_field {
+                        if state.editing_field {
                             let confirmed = present_confirmation(terminal)?;
                             if confirmed {
                                 // Save changes
-                                unsaved_changes = false;
-                                for (field, value) in config_fields.iter().zip(config_widget.iter()) {
+                                state.unsaved_changes = false;
+                                for (field, value) in state.config_fields.iter().zip(state.config_widgets.iter()) {
                                     config[field] = serde_json::Value::String(value.clone());
                                 }
                             }
-                            editing_field = false;
+                            state.editing_field = false;
                         }
                     }
                     KeyCode::Char('c') | KeyCode::Char('C') => {
-                        if !editing_field {
+                        if !state.editing_field {
                         }
                     }
                     KeyCode::Char('b') | KeyCode::Char('B') => {
-                        if !editing_field {
+                        if !state.editing_field {
                             break;
                         }
                     }
                     KeyCode::Char('e') | KeyCode::Char('E') => {
-                        if !editing_field {
-                            editing_field = !editing_field;
+                        if !state.editing_field {
+                            state.editing_field = !state.editing_field;
                         }
                     }
                     KeyCode::Char(c) => {
-                        if editing_field {
-                            unsaved_changes = true;
-                            config_widget[selected_field].push(c);
+                        if state.editing_field {
+                            state.unsaved_changes = true;
+                            state.config_widgets[state.selected_field].push(c);
                         }
                     }
                     KeyCode::Backspace => {
-                        if editing_field {
-                            unsaved_changes = true;
-                            config_widget[selected_field].pop();
+                        if state.editing_field {
+                            state.unsaved_changes = true;
+                            state.config_widgets[state.selected_field].pop();
                         }
                     }
                     KeyCode::Esc => {
-                        if editing_field {
-                            if unsaved_changes {
+                        if state.editing_field {
+                            if state.unsaved_changes {
                                 let abort_confirmed = present_abort_confirmation(terminal)?;
                                 if abort_confirmed {
-                                    editing_field = false;
+                                    state.editing_field = false;
                                 }
                             } else {
-                                editing_field = false;
+                                state.editing_field = false;
                             }
                         } else {
                             break;
                         }
                     }
                     KeyCode::Tab => {
-                        if editing_field {
-                            selected_field = (selected_field + 1) % config_fields.len();
+                        if state.editing_field {
+                            state.selected_field = (state.selected_field + 1) % state.config_fields.len();
                         }
                     }
                     KeyCode::BackTab => {
-                        if editing_field {
-                            if selected_field == 0 {
-                                selected_field = config_fields.len() - 1;
+                        if state.editing_field {
+                            if state.selected_field == 0 {
+                                state.selected_field = state.config_fields.len() - 1;
                             } else {
-                                selected_field -= 1;
+                                state.selected_field -= 1;
                             }
                         }
                     }
@@ -252,6 +153,133 @@ fn view_config(
     terminal.clear()?;
     Ok(())
 }
+
+struct MenuState<'a> {
+    selected_field: usize,
+    editing_field: bool,
+    unsaved_changes: bool,
+    scroll_offset: usize,
+    required_config: &'a ModelRequiredConfig,
+    config_fields: Vec<String>,
+    config_widgets: Vec<String>,
+}
+fn present_menu(terminal: &mut Terminal<CrosstermBackend<io::Stdout>>, state: &mut MenuState) -> Result<(), Box<dyn Error>> {
+    terminal.draw(|frame| {
+        let size = frame.size();
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints([
+                Constraint::Length(3), // View Model title
+                Constraint::Percentage(100),
+            ])
+            .split(size);
+        let block = Block::default()
+            .borders(Borders::ALL)
+            .title("View Model")
+            .style(Style::default()
+                .fg(Color::White)
+                .add_modifier(Modifier::BOLD));
+        frame.render_widget(block, chunks[0]);
+
+        let h_chunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([Constraint::Percentage(20), Constraint::Percentage(80)].as_ref())
+            .split(chunks[1]);
+
+        // Actions on left
+        let action_text = if state.editing_field {
+            "[S] Save [ESC] Stop Editing [B] Back"
+        } else {
+            "[E] Edit [B] Back"
+        };
+        let action_span = Span::styled(action_text, Style::default().fg(Color::White));
+        let action_block = Block::default().title(action_span).borders(Borders::ALL);
+        let action_paragraph_text = if state.editing_field {
+            "Editing"
+        } else {
+            ""
+        };
+        let action_paragraph_text_color = if state.editing_field {
+            Color::Yellow
+        } else {
+            Color::White
+        };
+        let action_paragraph = Paragraph::new(action_paragraph_text)
+            .style(Style::default().fg(action_paragraph_text_color))
+            .block(action_block);
+        frame.render_widget(action_paragraph, h_chunks[0]);
+
+        // Further split right side vertically
+        let v_chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([Constraint::Percentage(25), Constraint::Percentage(75)].as_ref())
+            .split(h_chunks[1]);
+
+        // Top right: Model static fields
+        let formatted_string = format!("{} ({})", state.required_config.name, state.required_config.id);
+        let question = Paragraph::new(Spans::from(vec![Span::raw(formatted_string)]))
+            .style(Style::default().fg(Color::White))
+            .alignment(Alignment::Center)
+            .block(Block::default()
+                .border_style(Style::default().fg(Color::White))
+                .borders(Borders::ALL)
+                .title(Span::styled("\nModel", Style::default()
+                    .fg(Color::Cyan)
+                    .add_modifier(Modifier::BOLD))
+            ));
+        frame.render_widget(question, v_chunks[0]);
+
+        // Bottom right: Model dynamic fields
+        let num_displayed_fields = std::cmp::min(5, state.config_fields.len() - state.scroll_offset);
+
+
+        let config_block = Block::default().title("Config").borders(Borders::ALL);
+        frame.render_widget(config_block, v_chunks[1]);
+
+        // Formatting dynamic properties
+        let dynamic_properties = state.config_fields.iter()
+            .skip(state.scroll_offset)
+            .take(num_displayed_fields)
+            .zip(state.config_widgets.iter().skip(state.scroll_offset).take(num_displayed_fields))
+            .enumerate()
+            .map(|(i, field)| {
+                let is_selected = state.scroll_offset + i == state.selected_field;
+                let field_style = if state.editing_field && is_selected {
+                    Style::default().fg(Color::Black)
+                } else {
+                    Style::default().fg(Color::White)
+                };
+                let value_style = if state.editing_field && is_selected {
+                    Style::default().fg(Color::Black).bg(Color::White)
+                } else {
+                    Style::default().fg(Color::White).bg(Color::Blue)
+                };
+                let field_span = Span::styled(format!("{:<15}", field.0), field_style);
+                let value_span = Span::styled(format!("{:<25}", state.config_widgets[state.scroll_offset + i]), value_style);
+                let spans = Spans::from(vec![field_span, Span::raw(" "), value_span]);
+                ListItem::new(spans)
+            })
+            .collect::<Vec<_>>();
+
+        // Further bottom right horizontally 
+        let bottom_right_h_chunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .margin(4)
+            .constraints([Constraint::Percentage(33), Constraint::Percentage(34), Constraint::Percentage(33)].as_ref())
+            .split(v_chunks[1]);
+
+        // List widget with centered content
+        let properties_widget = List::new(dynamic_properties)
+            .block(Block::default().borders(Borders::NONE).title("Configuration Parameters").title_alignment(Alignment::Center))
+            .style(Style::default().fg(Color::White))
+            .start_corner(Corner::TopLeft);
+
+        frame.render_widget(properties_widget, bottom_right_h_chunks[1]);
+    })?;
+    Ok(())
+}
+
 
 fn present_confirmation(terminal: &mut Terminal<CrosstermBackend<io::Stdout>>) -> Result<bool, Box<dyn Error>>{
     let mut confirmed = false;
